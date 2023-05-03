5e47cd03ad34914e1403058b765343bd188bd407
Fraser Curry <fraserjcurry@gmail.com>
2023-05-03 14:54:37 +0100

    Final training completed

--- Time Analysis.py
+++ Time Analysis.py
@@ -7,16 +7,17 @@ import matplotlib.pyplot as plt
 data_path_prefix = "D:/Uni Stuff/IP/Data/"
 
 with np.load(data_path_prefix + "Correction Times.npz", allow_pickle=True) as times:
-    image_undistortion_times = times["image_undistortion_times"]
-    bbox_undistortion_times = times["bbox_undistortion_times"]
+    time = times["bbox_undistortion_times"]
 
-total_times = np.add(image_undistortion_times, bbox_undistortion_times)
+#total_times = np.add(image_undistortion_times, bbox_undistortion_times)
 
 #print(np.min(bbox_undistortion_times))
 #print(np.max(bbox_undistortion_times))
 #print(np.mean(bbox_undistortion_times))
 
-plt.hist(total_times, bins=50)
-plt.xlabel("Total undistortion time per image ($s$)")
+print((time.size - np.count_nonzero(time)) / time.size)
+
+plt.hist(time, bins=50, range=[0,0.04])
+plt.xlabel("Bounding box generation time per image ($s$)")
 plt.ylabel("Frequency")
-plt.show()
+#plt.show()
